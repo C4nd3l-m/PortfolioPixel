07df98e7eee6fec65ff5ee95ebeb0bbafa0d3dba
C4nd3l-m <c4nd3l.m@gmail.com>
2025-02-15 23:48:36 -0300

    home

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,10 +1,20 @@
-const Home = () =>{
-  return(
+const Home = () => {
+  return (
     <>
-      <div>
-        holo
+      <div
+        className="w-full h-screen flex items-center justify-center bg-cover bg-center"
+        style={{ backgroundImage: "url('/bggif.gif')" }}
+      >
+        <span className="text-white text-2xl">
+          Welcome to my portfolio! I am Candela Villaverde 👾
+        </span>
+
+      </div>
+
+      <div className="text-center -mt-20">
+        <button title="Press start" type="button">Press Start</button>
       </div>
     </>
   )
 }
-export default Home;
+export default Home
