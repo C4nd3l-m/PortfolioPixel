c80f4880e9d862bcf87aac89de959fa885e537d1
C4nd3l-m <c4nd3l.m@gmail.com>
2025-02-17 21:31:29 -0300

    nav icons

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -1,20 +1,54 @@
 "use client"
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const NavBar = () => {
-    const pathName = usePathname() 
+    const pathName = usePathname()
 
-    if(pathName === "/") return null;
-    return(
-        <div> 
-            <Link href={"/"}>Home</Link>
+    if (pathName === "/") return null;
+    return (
+        <div className="fixed top-0 w-full bg-black text-white p-4 flex gap-6 justify-center items-center">
 
-            <Link href={"/about"}>About</Link>
+            <Link href={"/"} className="flex flex-col items-center">
+                <Image
+                    src="/icons/home.png"
+                    alt="Home Icon"
+                    width={24}  
+                    height={24} 
+                />
+                <span>Home</span>
+            </Link>
 
-            <Link href={"/projects"}>Projects</Link>
+            <Link href={"/about"} className="flex flex-col items-center">
+                <Image
+                    src="/icons/about.png"
+                    alt="About Icon"
+                    width={24}  
+                    height={24}
+                />
+                <span>About Me</span>
+            </Link>
 
-            <Link href={"/contact"}>Contact</Link>
+            <Link href={"/projects"} className="flex flex-col items-center">
+                <Image
+                    src="/icons/projects.png"
+                    alt="Projects Icon"
+                    width={24}  
+                    height={24}
+                />
+                <span>Projects</span>
+            </Link>
+
+            <Link href={"/contact"} className="flex flex-col items-center">
+                <Image 
+                    src="/icons/contact.png"
+                    alt="Contact Icon"
+                    width={24}  
+                    height={24}
+                />
+                <span>Contact</span>
+            </Link>
         </div>
     )
 }
