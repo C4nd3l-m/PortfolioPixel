1c17f6880e79eee6e17dda0af8fed8460e57a5e9
C4nd3l-m <c4nd3l.m@gmail.com>
2025-02-17 22:03:31 -0300

    footer

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Press_Start_2P } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,7 +36,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${pressStart.variable} antialiased`}
       >
         <NavBar/>
-        {children}
+        <main className="min-h-screen flex flex-col">{children}</main>
+        
+        <Footer/>
       </body>
     </html>
   );
---
+++ front/src/components/Footer.tsx
@@ -0,0 +1,50 @@
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Footer = () => {
+    const pathName = usePathname()
+
+    if (pathName === "/") return null;
+
+    return (
+        <footer className="bg-black text-white py-4 flex flex-col items-center gap-2">
+            <p className="text-xs">&copy; 2025 Candela Villaverde. All rights reserved.</p>
+
+            <div className="flex gap-4">
+                <Link href="https://www.linkedin.com/in/candela-villaverde-0a9027318/" target="_blank">
+                    <Image
+                        src="/icons/linkedin.gif"
+                        alt="LinkedIn Icon"
+                        width={24}
+                        height={24}
+                        className="hover:scale-110 transition-transform"
+                    />
+                </Link>
+
+                <Link href="https://github.com/C4nd3l-m" target="_blank">
+                    <Image
+                        src="/icons/github.gif"
+                        alt="GitHub Icon"
+                        width={24}
+                        height={24}
+                        className="hover:scale-110 transition-transform"
+                    />
+                </Link>
+
+                <Link href="mailto:c4nd3l.m@gmail.com">
+                    <Image
+                        src="/icons/gmail.gif"
+                        alt="Gmail Icon"
+                        width={24}
+                        height={24}
+                        className="hover:scale-110 transition-transform"
+                    />
+                </Link>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
