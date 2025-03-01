6d54bdb587a066929a7eac7783be34f645dc5dfb
C4nd3l-m <c4nd3l.m@gmail.com>
2025-02-28 22:36:56 -0300

    fix projects

--- front/src/app/projects/page.tsx
+++ front/src/app/projects/page.tsx
@@ -5,20 +5,24 @@ import arrProjects from "@/helpers/projects";
 const Projects = () => {
     return (
         <>
-            <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-20 mx-auto max-w-5xl">
                 {arrProjects.map((project, index) => (
-                    <div key={index}>
-                        <Link href={project.href}>
-                            <Image className="mt-40 ml-20 mb-10 mr-5"
+                    <div key={index} className="flex flex-col items-center bg-gray-800 rounded-lg p-4 shadow-lg 
+            hover:scale-105 transition-transform hover:bg-gray-700">
+                        <Link href={project.href} target="_blank" rel="noopener noreferrer">
+                            <Image
                                 src={project.src}
                                 width={200}
                                 height={200}
                                 alt={project.alt}
+                                className="rounded-md object-contain"
                             />
                         </Link>
+                        <h3 className="text-white text-lg font-bold mt-4">{project.alt}</h3>
                     </div>
                 ))}
             </div>
+
         </>
     )
 }
