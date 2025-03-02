fac59e8d4880d4fa6de7be30e630b70893effaed
C4nd3l-m <c4nd3l.m@gmail.com>
2025-03-01 21:54:01 -0300

    projects

--- front/src/app/projects/page.tsx
+++ front/src/app/projects/page.tsx
@@ -5,10 +5,10 @@ import arrProjects from "@/helpers/projects";
 const Projects = () => {
     return (
         <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-20 mx-auto max-w-5xl">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-8 mt-40 mb-20 mx-auto max-w-5xl">
                 {arrProjects.map((project, index) => (
-                    <div key={index} className="flex flex-col items-center bg-gray-800 rounded-lg p-4 shadow-lg 
-            hover:scale-105 transition-transform hover:bg-gray-700">
+                    <div key={index} className="flex flex-col items-center rounded-lg p-4 shadow-lg 
+            hover:scale-105 transition-transform">
                         <Link href={project.href} target="_blank" rel="noopener noreferrer">
                             <Image
                                 src={project.src}
@@ -19,6 +19,7 @@ const Projects = () => {
                             />
                         </Link>
                         <h3 className="text-white text-lg font-bold mt-4">{project.alt}</h3>
+                        <p  className="text-gray-300 text-sm mt-2 text-center">{project.description}</p>
                     </div>
                 ))}
             </div>
--- front/src/helpers/projects.ts
+++ front/src/helpers/projects.ts
@@ -2,22 +2,26 @@ const arrProjects = [
     {
         src: "/appsLogos/NextByte.png",
         alt: "NextByte",
-        href: "https://e-commerce-nextbyte.vercel.app/"
+        href: "https://e-commerce-nextbyte.vercel.app/",
+        description: "E-commerce with login, shopping cart, user dashboard with purchase history."
     },
     {
         src: "/appsLogos/StreamBox.png",
         alt: "StreamBox",
-        href: "https://youtu.be/08h0Vb00YWs"
+        href: "https://youtu.be/08h0Vb00YWs",
+        description: "Website to view movie information and add your favorite movies."
     },
     {
         src: "/appsLogos/Itchy.png",
         alt: "Itchy & Scratchy land",
-        href: "https://youtu.be/7hd-a6vghEo"
+        href: "https://youtu.be/7hd-a6vghEo",
+        description: "Website to reserve tickets for amusement park attractions."
     },
     {
         src: "/appsLogos/Active.png",
         alt: "Active 365",
-        href: "https://youtu.be/RVm0CKElVlY"
+        href: "https://youtu.be/RVm0CKElVlY",
+        description: "Fitness platform that combines e-commerce for gym products, reservation of specialized classes and a search engine for nearby gyms."
     }
 ];
 
